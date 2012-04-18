f01312bff111067f2cd89d5447f487ac1f01bfe3
Simon Menke <simon.menke@gmail.com>
2012-04-18 15:39:57 +0200

    Fixed homebrew installer

--- homebrew/butler.rb
+++ homebrew/butler.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class Butler < Formula
-  url 'https://raw.github.com/fd/butler/master/butler-0.0.1.tar.gz'
+  url 'https://raw.github.com/fd/butler/master/homebrew/butler-0.0.1.tar.gz'
   homepage 'http://github.com/fd/butler'
   md5 'e3dafa46a6bc08d056e1ee1a831a0e5f'
 
